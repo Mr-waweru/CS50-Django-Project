1742b6ba48d4c9b53228c77e0f4735890c5bc150
Mr-waweru <gitaugeorge001@gmail.com>
2024-08-31 23:29:48 +0300

    Fixed views.py in def flight

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, Http404
 from django.urls import reverse
+from django import forms
 
 from .models import Flight, Passenger
 
@@ -12,8 +13,11 @@ def index(request):
     })
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id) #Alt.get(id=flight_id)
-    return render (request, "flights/flight.html", {
+    try:
+        flight = Flight.objects.get(id=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight not found.")
+    return render(request, "flights/flight.html", {
         "flight": flight,
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
@@ -21,7 +25,14 @@ def flight(request, flight_id):
 
 def book(request, flight_id):
     if request.method == "POST":
-        flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        try:
+            passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+            flight = Flight.objects.get(pk=flight_id)
+        except KeyError:
+            return HttpResponseBadRequest("Bad Request: no flight chosen")
+        except Flight.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: flight does not exist")
+        except Passenger.DoesNotExist:
+            return HttpResponseBadRequest("Bad Request: passenger does not exist")
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+        return HttpResponseRedirect(reverse("flights:flight", args=(flight_id,)))
